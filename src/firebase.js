776d5e52545ad87efafa542c5d35b96747438858
Gabriel Freire <gafrenes@gmail.com>
2026-09-10 19:29:56 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB2lGiUqguU-nb8EU_JAFO66S2hVUsHVls",
-  authDomain: "as2-gabriel-freire.firebaseapp.com",
-  projectId: "as2-gabriel-freire",
-  storageBucket: "as2-gabriel-freire.firebasestorage.app",
-  messagingSenderId: "195015847340",
-  appId: "1:195015847340:web:eaac85c615b356e133de09"
+  apiKey: "AIzaSyDYWiT72DZx5O-zWNvf5OD1O7P8D-ZGN4k",
+  authDomain: "as2-gabriel-freire-devops.firebaseapp.com",
+  projectId: "as2-gabriel-freire-devops",
+  storageBucket: "as2-gabriel-freire-devops.firebasestorage.app",
+  messagingSenderId: "841403480791",
+  appId: "1:841403480791:web:c5b6695c2b35f69ac9ce92"
 };
 
 const app = initializeApp(firebaseConfig);
